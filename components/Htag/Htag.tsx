a843d8268b84bac85c396b035ee3b4a1482a8cff
duch2K <sarsenbek.d@gmail.com>
2022-01-20 18:17:31 +0600

    added Htag

---
+++ components/Htag/Htag.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import { HtagProps } from "./Htag.props";
+
+export const Htag: FC<HtagProps> = ({ tag, children }): JSX.Element => {
+  switch (tag) {
+    case 'h1':
+      return <h1>{children}</h1>;
+    case 'h2':
+      return <h2>{children}</h2>;
+    case 'h3':
+      return <h3>{children}</h3>;
+    default:
+      return <>{children}</>;
+  }
+};
---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from "next/document";
+
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initilProps = await Document.getInitialProps(ctx);
+    return { ...initilProps };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
